552c3f5dc211309bbd1a04912f9d840153e6ff18
Diogo Gomes <102877595+d1ogogomes@users.noreply.github.com>
2024-06-04 16:50:48 +0100

    update

--- main.js
+++ main.js
@@ -10,3 +10,55 @@ const config = {
     update: update
   }
 };
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('card', 'assets/images/carta.png');
+  this.load.image('bomb', 'assets/images/bomb.png');
+
+  this.load.image('number1', 'assets/images/carta.png');
+  this.load.image('number2', 'assets/images/carta.png');
+  this.load.image('number3', 'assets/images/carta.png');
+}
+
+function create() {
+  // Inicializar o tabuleiro do jogo e outros elementos
+  this.gameBoard = new GameBoard(this);
+  this.gameBoard.createBoard();
+  
+  this.score = 0;
+  this.scoreText = this.add.text(10, 10, 'Score: 0', { fontSize: '32px', fill: '#fff' });
+  
+  this.resetButton = this.add.text(700, 10, 'Reset', { fontSize: '32px', fill: '#fff' });
+  this.resetButton.setInteractive();
+  this.resetButton.on('pointerdown', () => this.resetGame());
+}
+
+
+function update() {
+
+  this.gameBoard();
+
+
+
+}
+
+class GameBoard {
+  constructor(scene) {
+    this.scene = scene;
+    this.boardSize = 5;
+    this.grid = [];
+    this.initializeGrid();
+  }
+
+  initializeGrid() {
+    for (let row = 0; row < this.boardSize; row++) {
+      this.grid[row] = [];
+      for (let col = 0; col < this.boardSize; col++) {
+        this.grid[row][col] = { value: 1, isBomb: false, revealed: false };
+      }
+    }
+    this.assignValuesAndBombs();
+  }
+}
