57f61d0e8e20115d86056c9a26e014c77055d64e
Diogo Gomes <102877595+d1ogogomes@users.noreply.github.com>
2024-06-04 16:36:35 +0100

    Update main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+import Phaser from 'phaser';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
